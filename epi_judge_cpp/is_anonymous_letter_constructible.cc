d6ee234e9f1c6a00efe5d04cc7d39680cd9c60c6
Huy Phan <huyrse@gmail.com>
2023-02-21 16:28:19 -0600

    13.2

--- epi_judge_cpp/is_anonymous_letter_constructible.cc
+++ epi_judge_cpp/is_anonymous_letter_constructible.cc
@@ -5,8 +5,23 @@ using std::string;
 
 bool IsLetterConstructibleFromMagazine(const string& letter_text,
                                        const string& magazine_text) {
-  // TODO - you fill in here.
-  return true;
+  std::unordered_map<char, int> map;
+  for (const char& c : letter_text) {
+    ++map[c];
+  }
+  for (const char& c : magazine_text) {
+    auto it = map.find(c);
+    if (it != map.cend()) {
+      --it->second;
+      if (it->second == 0) {
+        map.erase(it);
+        if (map.empty()) 
+          break;
+      }
+    }
+  }
+
+  return map.empty();
 }
 
 int main(int argc, char* argv[]) {
