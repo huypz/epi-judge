01bb4f766dde3517aa4f1f677479b5b96d9f38d1
Huy Phan <huyrse@gmail.com>
2023-02-25 06:32:13 -0600

    17.1

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -5,8 +5,18 @@ using std::vector;
 
 int NumCombinationsForFinalScore(int final_score,
                                  const vector<int>& individual_play_scores) {
-  // TODO - you fill in here.
-  return 0;
+  vector<vector<int>> dp(individual_play_scores.size(), vector<int>(final_score + 1, 0));
+
+  for (int i = 0; i < individual_play_scores.size(); i++) {
+    dp[i][0] = 1;
+    for (int j = 1; j <= final_score; j++) {
+      int without_this_play = i >= 1 ? dp[i-1][j] : 0;
+      int with_this_play = j >= individual_play_scores[i] ? dp[i][j - individual_play_scores[i]] : 0;
+      dp[i][j] = without_this_play + with_this_play;
+    }
+  }
+
+  return dp.back().back();
 }
 
 int main(int argc, char* argv[]) {
