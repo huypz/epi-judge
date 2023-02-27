3835adff15adb006f9663a6ed8c92657dbcbef92
Huy Phan <huyrse@gmail.com>
2023-02-27 11:49:20 -0600

    18.4

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -3,9 +3,24 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+bool HasTwoSum(vector<int> A, int t) {
+  int i = 0;
+  int j = A.size() - 1;
+  while (i <= j) {
+    if (A[i] + A[j] == t) return true;
+    else if (A[i] + A[j] < t) i++;
+    else j--;
+  }
+  return false;
+}
+
 bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+  sort(A.begin(), A.end());
+
+  for (int a : A) {
+    if (HasTwoSum(A, t - a)) return true;
+  }
+  return false;
 }
 
 int main(int argc, char* argv[]) {
